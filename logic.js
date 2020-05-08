41b3f9210768c67f06aedb25c1175ee3b6b70b46
Mario Kozic <kun.zizinho@gmail.com>
2020-05-08 01:13:38 -0400

    display questions/game start

--- logic.js
+++ logic.js
@@ -85,3 +85,63 @@ startBtn.addEventListener('click', function () {
     document.querySelector('.QnA').style.display = 'none';
     displayQuestions(questionIndex);
 })
+
+var questionIndex = 0;
+
+//funkcija za ispis pitanja i mogucih odgovora
+function displayQuestions() {
+
+    questionsEl.textContent = "";
+    var question = questions[questionIndex];
+    var questionDiv = document.createElement("div");
+    var questionText = document.createElement("h2");
+    questionText.textContent = question.title;
+    questionDiv.appendChild(questionText)
+
+    for (var i = 0; i < question.choices.length; i++) {
+        var option = document.createElement("button");
+        option.textContent = question.choices[i];
+        option.setAttribute("class", "option");
+        option.addEventListener("click", function (e) {
+            var optionClicked = (e.target.innerHTML);
+
+            if (optionClicked === questions[questionIndex].answer) {
+                alert("Correct!");
+                displayQuestions(questionIndex++);
+            } else {
+                alert("Incorrect!");
+                displayQuestions(questionIndex++);
+                secondsLeft -= 10;
+            }
+
+        });
+        if (questionIndex == questions.length - 1) {
+            clearInterval(timerInterval);
+            document.querySelector('.Score').style.display = 'block';
+            document.querySelector(".Time").style.display = 'none';
+            highScoreEl.textContent = "YOUR SCORE IS " + secondsLeft;
+            return;
+        }
+        questionDiv.appendChild(option);
+    }
+    questionsEl.appendChild(questionDiv);
+}
+
+//funkcija za rezultate
+function renderScores() {
+    scoreList.innerHTML = "";
+
+    for (var x = 0; x < userInitials.length; x++) {
+        var letters = userInitials[x];
+
+        var li = document.createElement("li");
+        li.textContent = letters;
+        li.setAttribute("data-index", x);
+
+        var button = document.createElement("button");
+        button.textContent = "Clear";
+
+        li.appendChild(button);
+        scoreList.appendChild(li);
+    }
+}
